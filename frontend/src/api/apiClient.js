556073ede018e8ca14fa61be13734e31fe26f448
Ander-17 <anderson.lopera@est.iudigital.edu.co>
2026-03-20 21:12:18 -0500

    Confirmación de puerto

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:4000/api' // Cambia el 4000 si tu backend usa otro puerto
+    baseURL: 'http://localhost:4000/api'
 });
 
 export default apiClient;
